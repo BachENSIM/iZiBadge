33a72e7026c925b0af2058d4a6dfdd4521815edf
BachENSIM <ducbachng97@gmail.com>
2022-05-18 20:57:57 +0200

    v1.0.13 - Manage User invites

---
+++ lib/components/table_list_user_form.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:izibagde/components/custom_colors.dart';
+import 'package:izibagde/model/database_test.dart';
+
+class TableUserForm extends StatefulWidget {
+  //const TableUserForm({Key? key}) : super(key: key);
+
+  @override
+  _TableUserFormState createState() => _TableUserFormState();
+}
+
+class _TableUserFormState extends State<TableUserForm> {
+  int? sortColumnIndex; //pour mettre en orde (sorting)
+  bool isAscending = false;
+  int no = 0;
+  final columns = ["Email d'invités", "État d'entrée"];
+  late List<String> lstEmail = [];
+  late List<bool> lstStatus = [];
+  late List<String> lstCombine = [];
+
+  @override
+  void initState() {
+    lstEmail = DatabaseTest.lstInviteChecked.keys.toList(growable: false);
+    lstStatus = DatabaseTest.lstInviteChecked.values.toList(growable: false);
+    for (int i = 0; i < lstStatus.length; i++) {
+      String mess = lstEmail[i] + "-" + lstStatus[i].toString();
+      lstCombine.add(mess);
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      sortAscending: isAscending,
+      sortColumnIndex: sortColumnIndex,
+      columns: getColumns(columns),
+      rows: getRows(lstCombine),
+      columnSpacing: 56,
+      //headingRowColor: MaterialStateColor.resolveWith((states) => Colors.lightBlueAccent),
+      headingTextStyle: const TextStyle(fontWeight: FontWeight.bold, color: Colors.black,fontSize: 18),
+      horizontalMargin: 40,
+
+    );
+  }
+
+  List<DataColumn> getColumns(List<String> columns) => columns
+      .map((String column) => DataColumn(label: Center(child: Text(column),), onSort: onSort))
+      .toList();
+
+  List<DataRow> getRows(List<String> combines) {
+    return combines.map((String combineItem) {
+      final cells = [
+        combineItem.split("-").first,
+        combineItem.split("-").last
+      ];
+      return DataRow(cells: getCells(cells));
+    }).toList();
+  }
+
+  List<DataCell> getCells(List<dynamic> cells) =>
+      cells.map((data) => DataCell(Center(child: Text('$data')))).toList();
+
+  void onSort(int columnIndex, bool ascending) {
+    if (columnIndex == 0) {
+      lstCombine.sort((user1, user2) => compareString(
+          ascending, user1.split("-").first, user2.split("-").first));
+    } else if (columnIndex == 1) {
+      lstCombine.sort((user1, user2) => compareString(
+          ascending, user1.split("-").last, user2.split("-").last));
+    }
+
+    setState(() {
+      sortColumnIndex = columnIndex;
+      isAscending = ascending;
+    });
+  }
+
+  int compareString(bool ascending, String value1, String value2) =>
+      ascending ? value1.compareTo(value2) : value2.compareTo(value1);
+}
---
+++ lib/screens/check_list_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:izibagde/components/table_list_user_form.dart';
+
+import '../components/check_list_form.dart';
+
+class CheckListUserScreen extends StatefulWidget {
+  //const CheckListUserScreen({Key? key}) : super(key: key);
+  late final String documentId;
+
+  CheckListUserScreen({required this.documentId});
+
+  @override
+  _CheckListUserScreenState createState() => _CheckListUserScreenState();
+}
+
+class _CheckListUserScreenState extends State<CheckListUserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Liste d'invitation"),
+        leadingWidth: 100,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.purple, Colors.blue],
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
+            ),
+          ),
+        ),
+        leading: ElevatedButton.icon(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: const Icon(Icons.arrow_left_sharp),
+            label: const Text("Back"),
+            style: ElevatedButton.styleFrom(
+                elevation: 0,
+                primary: Colors.transparent,
+                textStyle: const TextStyle(
+                    fontSize: 16, fontWeight: FontWeight.bold))),
+      ),
+      body:
+      SingleChildScrollView(
+        child:  TableUserForm()
+      )
+      /* CheckListForm(
+          documentId:
+              widget.documentId),*/ // Here the scanned result will be shown
+
+    );
+  }
+}
